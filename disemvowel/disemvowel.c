f6edecd9baf002dc6ff5ba3a23fe698a27804e6e
Francisco <fancisco.montanez96@gmail.com>
2017-09-26 20:57:52 -0500

    made changes to disemvowel.c and main.c. commiting for good habit.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,47 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "disemvowel.h"
 
+
+// return true if ke isnt a vowel false if it is
+bool notvowel(char ke) {
+
+  return (ke!='a'||ke!='e'||ke!='i'||ke!='o'||ke!='u'
+	  || ke!='A'||ke!='E'||ke!='I'||ke!='O'||ke!='U');
+    }
+
+// helper function for disemvowel
+char* disemvoweler(char* str) {
+  
+  int length = strlen(str);
+  int nonvowels = 0;
+  int j = 0;
+  char* newnovowels;
+
+// all nonvowels will be counted to have size of new string
+  for (int i = 0; i < length; i++) {
+    if(notvowel(str[i])) {
+      nonvowels++;
+    }
+  }
+
+  // what will be returned
+  newnovowels = (char*)calloc(nonvowels + 1, sizeof(char));
+
+  for (int i = 0; i != '\0' &&  i < length; i++) {
+    if (notvowel(str[i]) {
+	newnovowels[j] = str[i];
+	j++;
+      }
+  }
+  free(str);
+  newnovowel[j]='\0';
+  return newnonvowel;
+}
+
+//disemvowels strings; main function
 char* disemvowel(char* str) {
-  return (char*) "";
+  return disemvoweler(str);
 }
